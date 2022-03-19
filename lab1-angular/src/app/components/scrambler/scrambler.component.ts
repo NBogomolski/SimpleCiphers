cf5e747dddaee8184d336160dc8f193775ffec3d
h1dd3ngod <71835824+h1dd3ngod@users.noreply.github.com>
2022-03-19 14:31:52 +0300

    columnDecrypt()

--- lab1-angular/src/app/components/scrambler/scrambler.component.ts
+++ lab1-angular/src/app/components/scrambler/scrambler.component.ts
@@ -16,9 +16,13 @@ export class ScramblerComponent {
   toPerform: any;
   afterCalc: any;
   key: any;
-  blockwidth: any;
+  blockWidth: any;
   @Input() method: Method | undefined;
-  @Input() isEncrypt: boolean | undefined;
+  @Input() isEncrypt: boolean | undefined = true;
+
+  onToggleChange() {
+    this.afterCalc = "";
+  }
 
   calc() {
     this.toPerform = this.toPerform.replace(/\s/g, '');
@@ -43,7 +47,7 @@ export class ScramblerComponent {
 
   columnEncrypt(): string {
     let encrypted = '';
-    let precedence: number[] = this.setPresedence(this.key);
+    let precedence: number[] = this.setPrecedence(this.key);
     const arrHeight = Math.ceil(this.toPerform.length / this.key.length);
     let matrix = [];
     let startInd = 0;
@@ -68,7 +72,7 @@ export class ScramblerComponent {
   }
 
   //the lower the number - the more the presedence
-  setPresedence(key: string): Array<number> {
+  setPrecedence(key: string): Array<number> {
     let sortedKey = [...key].sort();
     let sortedInd = 0,
       priorityInd = 0;
@@ -82,7 +86,30 @@ export class ScramblerComponent {
     return precedence;
   }
 
-  columnDecrypt() {}
+  columnDecrypt(): string {
+    let message = '';
+    let keyPrecedence = this.setPrecedence(this.key);
+    const arrHeight = Math.ceil(this.toPerform.length / this.key.length);
+    const fullColumnsLength = this.toPerform.length - this.key.length * (arrHeight-1);
+    let matrix = [];
+    for (let i = 0; i < arrHeight; i++) {
+      matrix[i] = new Array(this.key.length).fill('');
+    }
+    for (let j = 0, encryptedInd = 0; j < this.key.length; j++) {
+      let soughtInd = keyPrecedence.indexOf(j);
+      for (let i = 0; i < arrHeight; i++) {
+        if ((i == arrHeight-1) && (soughtInd >= fullColumnsLength)) {
+          break;
+        }
+        matrix[i][soughtInd] = this.toPerform.charAt(encryptedInd++);
+      }
+    }
+    for (let i = 0; i < arrHeight; i++) {
+        message += matrix[i].join('');
+    }
+
+    return message;
+  }
   visionerEncrypt() {}
   visionerDecrypt() {}
   playfairEncrypt() {}
